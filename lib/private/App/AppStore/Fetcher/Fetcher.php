88fff075a0f67e43adaaaa596f3019e037ce6633
Roeland Jago Douma <roeland@famdouma.nl>
2016-12-22 09:46:10 +0100

    Add ETag validation to appstore requests
    * If the ETag if present store it
    * If a stored ETag is present then pass it along (with the original
    response) to get
    * Add tests
    * Added files to classmap
    
    Signed-off-by: Roeland Jago Douma <roeland@famdouma.nl>

--- lib/private/App/AppStore/Fetcher/Fetcher.php
+++ lib/private/App/AppStore/Fetcher/Fetcher.php
@@ -21,6 +21,8 @@
 
 namespace OC\App\AppStore\Fetcher;
 
+use OC\App\AppStore\Fetcher\Exception\NotModifiedException;
+use OCP\AppFramework\Http;
 use OCP\AppFramework\Utility\ITimeFactory;
 use OCP\Files\IAppData;
 use OCP\Files\NotFoundException;
@@ -62,15 +64,38 @@ abstract class Fetcher {
 	/**
 	 * Fetches the response from the server
 	 *
+	 * @param string $ETag
+	 * @param string $content
+	 *
 	 * @return array
+	 * @throws NotModifiedException
 	 */
-	protected function fetch() {
+	protected function fetch($ETag, $content) {
+		$options = [];
+
+		if ($ETag !== null) {
+			$options['headers'] = [
+				'If-None-Match' => $ETag,
+			];
+		}
+
 		$client = $this->clientService->newClient();
-		$response = $client->get($this->endpointUrl);
+		$response = $client->get($this->endpointUrl, $options);
+
 		$responseJson = [];
-		$responseJson['data'] = json_decode($response->getBody(), true);
+		if ($response->getStatusCode() === Http::STATUS_NOT_MODIFIED) {
+			$responseJson['data'] = json_decode($content, true);
+		} else {
+			$responseJson['data'] = json_decode($response->getBody(), true);
+			$ETag = $response->getHeader('ETag');
+		}
+
 		$responseJson['timestamp'] = $this->timeFactory->getTime();
 		$responseJson['ncversion'] = $this->config->getSystemValue('version');
+		if ($ETag !== '') {
+			$responseJson['ETag'] = $ETag;
+		}
+
 		return $responseJson;
 	}
 
@@ -81,6 +106,9 @@ abstract class Fetcher {
 	 */
 	 public function get() {
 		$rootFolder = $this->appData->getFolder('/');
+
+		$ETag = '';
+		$content = '';
 
 		try {
 			// File does already exists
@@ -95,6 +123,11 @@ abstract class Fetcher {
 					isset($jsonBlob['ncversion']) && $jsonBlob['ncversion'] === $this->config->getSystemValue('version', '0.0.0')) {
 					return $jsonBlob['data'];
 				}
+
+				if (isset($jsonBlob['ETag'])) {
+					$ETag = $jsonBlob['ETag'];
+					$content = json_encode($jsonBlob['data']);
+				}
 			}
 		} catch (NotFoundException $e) {
 			// File does not already exists
@@ -103,7 +136,12 @@ abstract class Fetcher {
 
 		// Refresh the file content
 		try {
-			$responseJson = $this->fetch();
+			$responseJson = $this->fetch($ETag, $content);
+			$file->putContent(json_encode($responseJson));
+			return json_decode($file->getContent(), true)['data'];
+		} catch (NotModifiedException $e) {
+			$responseJson = json_decode($file->getContent(), true);
+			$responseJson['timestamp'] = $this->timeFactory->getTime();
 			$file->putContent(json_encode($responseJson));
 			return json_decode($file->getContent(), true)['data'];
 		} catch (\Exception $e) {
